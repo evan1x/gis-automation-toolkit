1c6d120c2e4fc295fee818a0674bf7f1ced27bef
GIS <gis@arran-elderslie.ca>
2025-06-04 09:56:25 -0400

    feat(scripts): add spatial join and buffer tools

--- scripts/Delete Empty Fields.py
+++ scripts/Delete Empty Fields.py
@@ -8,7 +8,7 @@ fields = arcpy.ListFields(fc)
 
 for field in fields:
     if field.type not in ['OID', 'Geometry']:
-        values = [row[0] for row in arcpy.da.SearchCursor(fc, field_name)]
+        values = [row[0] for row in arcpy.da.SearchCursor(fc, field.name)]
         if all(v in (None, '', ' ') for v in values):
             print(f"Deleting empty field: {field.name}")
             arcpy.DeleteField_management(fc, field.name)
--- scripts/List All Feature Classes in a GBD.py
+++ scripts/List All Feature Classes in a GBD.py
@@ -2,4 +2,4 @@ import arcpy
 
 arcpy.env.workspace = "path_to.gbd" # Replace with your path
 fcs = arcpy.ListFeatureClasses()
-print(fcs)
+print(fcs) 
